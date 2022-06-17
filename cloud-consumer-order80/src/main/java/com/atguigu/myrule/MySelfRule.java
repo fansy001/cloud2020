3c46af76c3ae3015b50d451b8c9c6f3e25d5c087
fansy001 <fsy_work@163.com>
2022-06-17 17:49:11 +0800

    修改代码 添加注释

--- cloud-consumer-order80/src/main/java/com/atguigu/myrule/MySelfRule.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/myrule/MySelfRule.java
@@ -1,9 +1,6 @@
 package com.atguigu.myrule;
 
-import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RandomRule;
-import com.netflix.loadbalancer.RetryRule;
-import com.netflix.loadbalancer.RoundRobinRule;
+import com.netflix.loadbalancer.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,5 +17,10 @@ public class MySelfRule
         return new RoundRobinRule();//轮询  默认轮询
 //        return new RandomRule();//定义为随机
 //        return new RetryRule();//先按照轮询策略，如果获取服务失败则在指定的时间内会进行重试，获取当前可用的服务然后进行轮询
+//        return new WeightedResponseTimeRule();//对RoundRobinRule的扩展，响应速度越快的实例选择权重越大，越容易被选择
+//        return new BestAvailableRule();//会先过滤掉由于多次访问故障而处于断路器跳闸状态的服务，然后选择一个并发量最小的服务
+//        return new AvailabilityFilteringRule();//先过滤掉故障实例，再选择并发较小的实例
+//        return new ZoneAvoidanceRule();//默认规则,复合判断server所在区域的性能和server的可用性选择服务器
+
     }
 }
