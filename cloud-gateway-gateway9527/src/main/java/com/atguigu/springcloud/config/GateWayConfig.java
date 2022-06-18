b6244f3512fa65d6b7009d76aea14746018478cb
fansy001 <fsy_work@163.com>
2022-06-18 18:31:54 +0800

    gateway网关 添加注释

--- cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/config/GateWayConfig.java
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/config/GateWayConfig.java
@@ -6,17 +6,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * 配置了一个id为route-name的路由规则，
+ *     当访问地址 http://localhost:9527/guonei时会自动转发到地址：http://news.baidu.com/guonei
  * @auther zzyy
  * @create 2020-02-21 11:42
  */
 @Configuration
 public class GateWayConfig
 {
+    /**
+     * gateway网关实现有两种方式
+     *      方式一：在application.yml文件中配置即可
+     *      方式二：写代码，如本方法
+     * @param routeLocatorBuilder
+     * @return
+     */
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder)
     {
         RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
 
+        //参数一：代表application.yml配置文件中的 routes：id
+        //参数二：代表application.yml配置文件中的 routes：predicates: - Path
+        //参数三：代表application.yml配置文件中的 routes：uri
         routes.route("path_route_atguigu",
                 r -> r.path("/guonei")
                         .uri("http://news.baidu.com/guonei")).build();
--- cloud-gateway-gateway9527/src/test/java/T2.java
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -10,6 +10,8 @@ public class T2
     {
         ZonedDateTime zbj = ZonedDateTime.now(); // 默认时区
         System.out.println(zbj);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
 
        //2020-02-21T15:51:37.485+08:00[Asia/Shanghai]
     }
