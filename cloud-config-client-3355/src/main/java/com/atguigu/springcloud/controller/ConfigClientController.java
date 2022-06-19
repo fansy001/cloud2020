cca233eb375b266f360faff206bd0f8e1d2ccd3b
fansy001 <fsy_work@163.com>
2022-06-19 12:58:07 +0800

    springcloud config配置中心 添加注释

--- cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -13,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class ConfigClientController
 {
+    /**
+     * 根据bootstrap.yml配置文件可知：连接springcloud配置中心服务端(http://config-3344.com:3344)，然后由
+     * springcloud配置中心服务端去读取它连接的github上的配置文件，注入到configInfo变量中
+     *
+     */
     @Value("${config.info}")
     private String configInfo;
 
